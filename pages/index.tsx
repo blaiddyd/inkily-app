91b4130ed7f5563560f80572ede7a845a4e8f4f3
blaiddyd <bukulmezekin@gmail.com>
2020-12-27 17:50:36 +0800

    set app typescript and eslint

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Head from "next/head";
+
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <Head>
+        <title>Inkily</title>
+      </Head>
+      <h1>HELLO!</h1>
+    </div>
+  );
+}
